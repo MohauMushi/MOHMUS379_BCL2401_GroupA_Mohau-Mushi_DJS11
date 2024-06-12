bbee8bfe4e0b2f7fe10ea97e4c3a7f1ba71a590e
Mohau Mushi <david1mushi1@gmail.com>
2024-06-12 20:58:18 +0200

    Updated Navbar component to add responsive design and toggle menu functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom";
-import { FaHome, FaHeart } from "react-icons/fa";
+import { FaHome, FaHeart, FaBars } from "react-icons/fa";
 import "./Navbar.css";
 import Logo from "../../assets/sound.png";
+import { useState } from 'react';
+
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+  
   return (
     <header>
       <nav className="navbar">
@@ -11,17 +19,20 @@ const Navbar = () => {
           <img src={Logo} alt="logo" className="logo-image" />
           <span className="logo-name">PodStream</span>
         </Link>
-        
-        <div className="nav-links">
-        <Link to="/" className="nav-link">
-          <FaHome /> Home
-        </Link>
-
-        <Link to="/favourites" className="nav-link">
-          <FaHeart /> Favourites
-        </Link>
+  
+        <div className={`nav-links ${showMenu ? 'show' : ''}`}>
+          <Link to="/" className="nav-link">
+            <FaHome /> Home
+          </Link>
+  
+          <Link to="/favourites" className="nav-link">
+            <FaHeart /> Favourites
+          </Link>
         </div>
-        
+  
+        <div className="menu-icon" onClick={toggleMenu}>
+          <FaBars />
+        </div>
       </nav>
     </header>
   );
