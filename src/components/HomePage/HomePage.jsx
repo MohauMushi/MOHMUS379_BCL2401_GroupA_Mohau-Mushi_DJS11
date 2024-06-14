ff48be0feb963f4c9745bfbb87141913e20628a6
Mohau Mushi <david1mushi1@gmail.com>
2024-06-14 13:56:06 +0200

    Updated HomePage.css and HomePage.jsx: added new CSS classes, modified existing styles, and added code blocks in JSX

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,11 +2,12 @@ import { useState, useEffect } from "react";
 import { fetchPodcasts } from "../../api/api";
 import { CircularProgress } from "@material-ui/core";
 import "./HomePage.css";
+import { mapGenres } from "../../utils/helperFunctions";
 
 const HomePage = () => {
   const [shows, setShows] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [showFullDescription, setShowFullDescription] = useState({});
+  // const [showFullDescription, setShowFullDescription] = useState({});
 
   useEffect(() => {
     const getPodcasts = async () => {
@@ -18,12 +19,6 @@ const HomePage = () => {
     getPodcasts();
   }, []);
 
-  const toggleDescription = (showId) => {
-    setShowFullDescription((prevState) => ({
-      ...prevState,
-      [showId]: !prevState[showId],
-    }));
-  };
 
   if (loading) {
     return <CircularProgress className="circular-progress"/>;
@@ -42,15 +37,17 @@ const HomePage = () => {
             />
             <div className="show-content">
               <h2 className="show-title">{show.title}</h2>
-              <p className="show-description">
+              <p className="show-line"></p>
+              {/* <p className="show-description">
                 {showFullDescription[show.id]
                   ? show.description
                   : `${show.description.slice(0, 140)}...`}
-              </p>
+              </p> */}
               <p className="show-seasons">Seasons: {show.seasons}</p>
               <p className="show-updated">
                 Last Updated: {new Date(show.updated).toLocaleDateString()}
               </p>
+              <p className="show-genres">{mapGenres(show.genres)}</p>
             </div>
           </div>
         ))}
