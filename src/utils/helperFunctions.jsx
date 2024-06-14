49433e5351eabafd97bcdc5b77681cf90cb66a8c
Mohau Mushi <david1mushi1@gmail.com>
2024-06-14 13:51:39 +0200

    added genre titles and mapping function

---
+++ src/utils/helperFunctions.jsx
@@ -0,0 +1,15 @@
+export const genreTitles = {
+	1 :	"Personal Growth",
+    2 :  "Investigative Journalism",
+    3 :  "History",
+    4 :  "Comedy",
+    5 :  "Entertainment",
+    6 :  "Business",
+    7 :  "Fiction",
+    8 :  "News",
+    9 :  "Kids and Family",
+}
+
+export const mapGenres = (genres) => {
+	return  genres.map((genre) => Object.values(genreTitles)[genre - 1]).join(", ")
+}
