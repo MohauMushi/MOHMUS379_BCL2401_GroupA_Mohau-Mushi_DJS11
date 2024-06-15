7ddb09914441c47c0a3a7da64fbfbe16cb7386f0
Mohau Mushi <david1mushi1@gmail.com>
2024-06-16 00:10:59 +0200

    Update Footer component: rename CSS class from 'footer' to '.footer', add 'bottom: 0' property, and add className to JSX element

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,6 @@ import "./Footer.css"
 
 export default function Footer() {
     return (
-        <footer>&#169; 2024 PodStream</footer>
+        <footer className="footer">&#169; 2024 PodStream</footer>
     )
 }
