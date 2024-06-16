a50cf13c803b0d3c7055b6a06e19bce05a9260f8
Mohau Mushi <david1mushi1@gmail.com>
2024-06-16 18:19:08 +0200

    Integrated PodcastPreview component with React Router enabling the display of detailed show information when navigating to the "/show/:id" route.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import Favourites from "./pages/Favourites";
 import Layout from "./components/Layout/Layout";
+import PodcastPreview from "./pages/PodcastPreview/PodcastPreview";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/favourites" element={<Favourites />} />
+          <Route path="/show/:id" element={<PodcastPreview />} />
         </Route>
       </Routes>
     </BrowserRouter>
