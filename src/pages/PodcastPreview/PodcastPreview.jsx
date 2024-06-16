5c7cffe912bb2fa2414ea23d8b95fe45a716f9c9
Mohau Mushi <david1mushi1@gmail.com>
2024-06-16 20:43:58 +0200

    Added back button to PodcastPreview component, using Navigate  to navigate to the homePage and updated CSS styles

--- src/pages/PodcastPreview/PodcastPreview.jsx
+++ src/pages/PodcastPreview/PodcastPreview.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { fetchShowDetails } from "../../api/api";
 import { CircularProgress } from "@material-ui/core";
 import "./PodcastPreview.css";
@@ -8,6 +8,8 @@ const PodcastPreview = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate(); 
+
 
   useEffect(() => {
     const getShowDetails = async () => {
@@ -27,35 +29,46 @@ const PodcastPreview = () => {
     return <div className="not-found">Show not found</div>;
   }
 
+  const handleBack = () => {
+    navigate(-1);
+  }
+
   return (
-    <div className="show-details">
-      <h2 className="show-title">{show.title}</h2>
-      <p className="show-description">{show.description}</p>
-      {show.seasons.map((season) => (
-        <div key={season.id} className="season-container">
-          <div className="season-details">
-            <img
-              className="season-image"
-              src={season.image}
-              alt={season.title}
-              title={season.title}
-            />
-            <div className="season-info">
-              <h3 className="season-title">{season.title}</h3>
-              <p className="season-episodes">Episodes: {season.episodes.length}</p>
+    <>
+      <Link onClick={handleBack} className="back-button">
+        &larr; <span>Back</span>
+      </Link>
+
+      <div className="show-details">
+        <h2 className="show-title">{show.title}</h2>
+        <p className="show-description">{show.description}</p>
+        {show.seasons.map((season) => (
+          <div key={season.id} className="season-container">
+            <div className="season-details">
+              <img
+                className="season-image"
+                src={season.image}
+                alt={season.title}
+                title={season.title}
+              />
+              <div className="season-info">
+                <h3 className="season-title">{season.title}</h3>
+                <p className="season-episodes">
+                  Episodes: {season.episodes.length}
+                </p>
+              </div>
+            </div>
+            <div className="episodes-list">
+              {season.episodes.map((episode) => (
+                <div key={episode.id} className="episode-card">
+                  <h4 className="episode-title">{episode.title}</h4>
+                </div>
+              ))}
             </div>
           </div>
-          <div className="episodes-list">
-            {season.episodes.map((episode) => (
-              <div key={episode.id} className="episode-card">
-                <h4 className="episode-title">{episode.title}</h4>
-        
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
