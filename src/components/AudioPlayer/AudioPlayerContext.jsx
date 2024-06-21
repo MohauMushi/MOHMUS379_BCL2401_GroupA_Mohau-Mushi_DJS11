ce2cb33e05797bce7578828542561abdd8d5caff
Mohau Mushi <david1mushi1@gmail.com>
2024-06-21 09:46:44 +0200

    created a new context for the audio player

---
+++ src/components/AudioPlayer/AudioPlayerContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useState, useContext } from 'react';
+
+const AudioPlayerContext = createContext();
+
+export const AudioPlayerProvider = ({ children }) => {
+  const [isPlayerOpen, setIsPlayerOpen] = useState(false);
+  const [currentEpisodeFile, setCurrentEpisodeFile] = useState(null);
+
+  const openPlayer = (episodeFile) => {
+    setCurrentEpisodeFile(episodeFile);
+    setIsPlayerOpen(true);
+  };
+
+  const closePlayer = () => {
+    setCurrentEpisodeFile(null);
+    setIsPlayerOpen(false);
+  };
+
+  return (
+    <AudioPlayerContext.Provider
+      value={{
+        isPlayerOpen,
+        currentEpisodeFile,
+        openPlayer,
+        closePlayer,
+      }}
+    >
+      {children}
+    </AudioPlayerContext.Provider>
+  );
+};
+
+export const useAudioPlayer = () => useContext(AudioPlayerContext);
