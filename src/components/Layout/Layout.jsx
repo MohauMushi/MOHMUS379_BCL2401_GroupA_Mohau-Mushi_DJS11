4180a6d82495d5feac955308b7f210dec25de8e5
Mohau Mushi <david1mushi1@gmail.com>
2024-06-21 09:53:24 +0200

    Implemented global AudioPlayer, for it to be accessed all over the web app

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,34 @@
+import React from 'react';
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import AudioPlayer from "../AudioPlayer/AudioPlayer";
+import { AudioPlayerProvider, useAudioPlayer } from '../AudioPlayer/AudioPlayerContext';
 
-export default function Layout() {
+const LayoutContent = () => {
+  const { isPlayerOpen, currentEpisodeFile, closePlayer } = useAudioPlayer();
+
   return (
     <div className="site-wrapper">
       <Navbar />
       <main>
         <Outlet />
       </main>
+      {isPlayerOpen && (
+        <AudioPlayer
+          episodeFile={currentEpisodeFile}
+          handleClosePlayer={closePlayer}
+        />
+      )}
       <Footer />
     </div>
+  );
+};
+
+export default function Layout() {
+  return (
+    <AudioPlayerProvider>
+      <LayoutContent />
+    </AudioPlayerProvider>
   );
 }
